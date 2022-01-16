1aa9d8055a5a199dd7158796eadbba2108bf1a88
BASS702 <hyl0714bass@gmail.com>
2022-01-17 00:58:08 +0800

    Phone git commit

---
+++ src/com/itheima/test1/TestPhone.java
@@ -0,0 +1,16 @@
+package com.itheima.test1;
+
+public class TestPhone {
+    public static void main(String[] args) {
+        Phone ph1 = new Phone();
+        ph1.setBrand("huawei");
+        ph1.setPrice(5555);
+        System.out.println(ph1.getBrand() + ph1.getPrice());
+        ph1.call("EG");
+        ph1.text("WEG");
+        Phone ph2 = new Phone("Iphone",7000);
+        System.out.println(ph2.getBrand()+ph2.getPrice());
+
+
+    }
+}
