55dd9c954362d50e16b04b046935edd09f5027e1
BASS702 <hyl0714bass@gmail.com>
2022-01-18 19:23:27 +0800

    Revert "123"
    This reverts commit 2838859b

--- src/com/itheima/test1/TestPhone.java
+++ src/com/itheima/test1/TestPhone.java
@@ -7,8 +7,12 @@ public class TestPhone {
         ph1.setPrice(5555);
         System.out.println(ph1.getBrand() + ph1.getPrice());
         ph1.call("EG");
-
-
+        ph1.text("WEG");
+        Phone ph2 = new Phone("Iphone",7000);
+        System.out.println(ph2.getBrand()+ph2.getPrice());
+        System.out.println("test 1");
+        System.out.println("test 2");
+        System.out.println("123");
 
 
 
